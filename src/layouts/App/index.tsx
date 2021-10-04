9e40db5edcb8831c87a4fc439d14501501e33a17
clevyoung <clevyoung@gmail.com>
2021-10-04 15:21:06 +0900

    리액트 라우팅 및 코드스플리팅 라이브러리 추가

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Switch>
+      <Route exact path="/" />
+    </Switch>
+  );
 };
 
 export default App;
